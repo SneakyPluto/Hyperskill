836c24d9e7da2c890da165d2a33cd4898c470f5b
Travis <sneakyplutosaturn@gmail.com>
2025-04-09 09:56:35 +1000

    "Once a string comes, everything after that is a string!"

---
+++ Topics/String/Extracting a specific character from a greeting message/src/Task.java
@@ -0,0 +1,14 @@
+// You can experiment here, it won’t be checked
+
+public class Task {
+  public static void main(String[] args) {
+    // put your code here
+    // "Once a string comes, everything after that is a string!"
+    System.out.println(1 + 1 + "2" + 2); // 222
+    System.out.println(2 + 2 + "2"); // 42
+    System.out.println("2" + 2 + 2); // 222
+    System.out.println("2" + 2 + 1 + 1); // 22211
+
+
+  }
+}
