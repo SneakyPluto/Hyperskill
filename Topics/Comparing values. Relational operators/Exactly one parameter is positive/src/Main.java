7e516e1327184949d8cdf9418cde3b7507266c02
Travis <sneakyplutosaturn@gmail.com>
2025-04-13 16:35:23 +1000

    Complete exactly one parameter is positive

--- Topics/Comparing values. Relational operators/Exactly one parameter is positive/src/Main.java
+++ Topics/Comparing values. Relational operators/Exactly one parameter is positive/src/Main.java
@@ -7,10 +7,21 @@ class Main {
         int num1 = scanner.nextInt();
         int num2 = scanner.nextInt();
         int num3 = scanner.nextInt();
+        int positiveCount = 0;
 
-        if (num1 > 0|| num2 > 0 || num3 > 0) {
-            System.out.println();
+
+
+        if (num1 > 0) {
+            positiveCount++; // Increment if num1 is positive
         }
-
+        if (num2 > 0) {
+            positiveCount++; // Increment if num2 is positive
+        }
+        if (num3 > 0) {
+            positiveCount++; // Increment if num3 is positive
+        }
+        boolean result = (positiveCount == 1);
+        System.out.println(result);
     }
+
 }
